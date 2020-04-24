ed82ea0108c7ee89ad69c87facdd49e1c9653e52
MaikPython <soeoskarpaul@gmail.com>
2020-04-24 13:28:47 +0300

    map done

--- SightMap/components/MapContainer.js
+++ SightMap/components/MapContainer.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, Dimensions } from 'react-native'
+import MapView from 'react-native-maps'
 
 const MapContainer = () => {
   return (
-    <View>
-      <Text>Map Container</Text>
+    <View style={styles.container}>
+      <MapView
+       region = 
+       {{
+          latitude: 59.4370,
+          longitude: 24.753574,
+          latitudeDelta: 0.0992,
+          longitudeDelta: 0.0421,
+       }} 
+      style={styles.mapStyle}
+      >
+      </MapView>
     </View>
   )
 }
 
 export default MapContainer
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+})
