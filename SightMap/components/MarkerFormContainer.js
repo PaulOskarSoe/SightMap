308fba744eeccd4733a74a9636a62c7ca75cfb42
MaikPython <soeoskarpaul@gmail.com>
2020-04-24 14:03:26 +0300

    first marker form done

--- SightMap/components/MarkerFormContainer.js
+++ SightMap/components/MarkerFormContainer.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, TextInput, Button, StyleSheet } from 'react-native'
 
 const MarkerFormContainer = () => {
+  
+  const [description, setDescription] = useState()
+  const [address, setAddress] = useState()
+
+  const onSightSeeingAdded = () => {
+    console.log('Added')
+  }
+
   return (
-    <View>
-      <Text>Marker continer</Text>
+    <View style={styles.container}>
+      <TextInput placeholder="address" style={styles.input} onChangeText={text => setAddress(text)} value={address}></TextInput>
+      <TextInput placeholder="description" style={styles.input} onChangeText={text => setDescription(text)} value={description}></TextInput>
+      <Button style={styles.button} title="Add new sightseeing" onPress={() => onSightSeeingAdded()}>
+      </Button>
     </View>
   )
 }
 
 export default MarkerFormContainer
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    marginBottom: 100,
+    backgroundColor: "#ff6347",
+    padding: 7,
+  },
+  text: {
+    color: 'white',
+  },
+  input: {
+    width:'80%', 
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1, 
+    margin: 10
+  }
+});
