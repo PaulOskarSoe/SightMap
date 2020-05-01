7f003adc6d1422c73edace18b827497e9b323949
MaikPython <soeoskarpaul@gmail.com>
2020-05-01 16:14:11 +0300

    add markers

--- SightMap/components/MapContainer.jsx
+++ SightMap/components/MapContainer.jsx
@@ -1,21 +1,48 @@
 /* eslint-disable no-use-before-define */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import { getMarkers } from '../services';
 
-const MapContainer = () => (
-  <View style={styles.container}>
-    <MapView
-      region={{
-        latitude: 59.4370,
-        longitude: 24.753574,
-        latitudeDelta: 0.0992,
-        longitudeDelta: 0.0421,
-      }}
-      style={styles.mapStyle}
-    />
-  </View>
-);
+const MapContainer = () => {
+  const [markers, updateMarkers] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getAllMarkers();
+    }, 5000);
+    getAllMarkers();
+    return () => clearInterval(interval);
+  }, []);
+
+  const getAllMarkers = async () => {
+    const newMarker = await getMarkers();
+    updateMarkers(newMarker.markers);
+  };
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        initialRegion={{
+          latitude: 59.4370,
+          longitude: 24.753574,
+          latitudeDelta: 0.0992,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.mapStyle}
+      >
+        {markers && markers.map((marker) => (
+          marker && (
+          <Marker
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            description={marker.description}
+          />
+          )
+        ))}
+      </MapView>
+    </View>
+  );
+};
 
 export default MapContainer;
 
