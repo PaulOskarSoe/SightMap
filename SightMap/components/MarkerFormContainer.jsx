ae81a97c54ff7f69bf6eb21f4cfbc6b0d60d82ee
Sanderzhanni <hannis@tlu.ee>
2020-05-19 02:40:10 +0300

    user object fixes

--- SightMap/components/MarkerFormContainer.jsx
+++ SightMap/components/MarkerFormContainer.jsx
@@ -1,19 +1,21 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-use-before-define */
-import React, { useState, useContext } from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {
-  View, TextInput, StyleSheet,
+  View, TextInput, StyleSheet, AsyncStorage,
 } from 'react-native';
+import {
+  Card, Input, Text, Icon, Button,
+} from 'react-native-elements';
 import { addMarker } from '../services';
-import UserContext from '../UserContext';
-import { Card, Input, Text, Icon, Button } from 'react-native-elements';
 
 const MarkerFormContainer = () => {
-  const user = useContext(UserContext);
+  const [user, setUser] = useState(null);
   const [description, setDescription] = useState();
   const [address, setAddress] = useState();
 
   const onSightSeeingAdded = () => {
+    console.debug('userID2', user);
     if (user && user._id && address) {
       setAddress('');
       setDescription('');
@@ -21,27 +23,44 @@ const MarkerFormContainer = () => {
     }
   };
 
-  return (
-  <View>
-  <Card title={
-    <View style={styles.cardTitle}>
-        <Text style={styles.cardText}>ADD THE NEW SPOT</Text>
-        <Icon
-          name='location'
-          type='evilicon'
-          color='#517fa4'
-          reverse='true'
-        />
-    </View>
-  } containerStyle={styles.card} >
+  const getUserFromStorage = async () => {
+    try {
+      const userFromStorage = await AsyncStorage.getItem('user');
+      const userObj = JSON.parse(userFromStorage);
+      setUser(userObj);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    getUserFromStorage();
+  }, []);
+
+  return (
     <View>
-      <Input placeholder="ex. Address, Town" onChangeText={(text) => setAddress(text)} value={address} />
-      <Input placeholder="description (max 128 characters)" onChangeText={(text) => setDescription(text)} value={description} maxLength={128} />
-      <Button raised="true" onPress={() => onSightSeeingAdded()} title="SAVE"></Button>
-     </View>
-   </Card>
-   </View>
+      <Card
+        title={(
+          <View style={styles.cardTitle}>
+            <Text style={styles.cardText}>ADD THE NEW SPOT</Text>
+            <Icon
+              name="location"
+              type="evilicon"
+              color="#517fa4"
+              reverse="true"
+            />
+          </View>
+  )}
+        containerStyle={styles.card}
+      >
+
+        <View>
+          <Input placeholder="ex. Address, Town" onChangeText={(text) => setAddress(text)} value={address} />
+          <Input placeholder="description (max 128 characters)" onChangeText={(text) => setDescription(text)} value={description} maxLength={128} />
+          <Button raised="true" onPress={() => onSightSeeingAdded()} title="SAVE" />
+        </View>
+      </Card>
+    </View>
   );
 };
 
@@ -64,13 +83,13 @@ const styles = StyleSheet.create({
     top: '50%',
   },
   cardTitle: {
-    flexDirection : 'column',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
   },
   cardText: {
     fontSize: 26,
-  }
+  },
 });
 
 export default MarkerFormContainer;
